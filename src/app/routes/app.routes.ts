09478a08617777b4ff819aedaebc77d92d69e683
piotr.maliga <piotr.maliga.work@gmail.com>
2025-01-17 17:13:09 +0100

    added structure refactor

---
+++ src/app/routes/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+
+const pageTitle = 'Game bar';
+
+export const routes: Routes = [
+  {
+    path: '',
+    title: `${pageTitle} | Home`,
+    loadComponent: () => import('@pages/home/home.component').then(m => m.HomePageComponent),
+  },
+  {
+    path: 'authorize',
+    title: `${pageTitle} | Sign in`,
+    loadComponent: () => import('@pages/auth/auth.component').then(m => m.AuthPageComponent),
+  },
+  {
+    path: '**',
+    title: `${pageTitle} | Not found`,
+    loadComponent: () =>
+      import('@pages/not-found/not-found.component').then(m => m.NotFoundPageComponent),
+  },
+];
