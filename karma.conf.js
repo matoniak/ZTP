54bd1ae1b86e64d90d414bf8c0e80082bc0dd10d
piotr.maliga <piotr.maliga.work@gmail.com>
2025-01-17 16:44:49 +0100

    added coverage setting into unit tests

---
+++ karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular'),
+      require('karma-chrome-launcher'),
+    ],
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    browsers: ['Chrome'],
+    singleRun: true,
+  });
+};
