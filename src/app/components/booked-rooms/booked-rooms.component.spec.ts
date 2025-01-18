486e7ea7fd2642359de6d0cf4b8816c6119906d8
piotr.maliga <piotr.maliga.work@gmail.com>
2025-01-18 12:28:36 +0100

    added unfinished unit test using util class

--- src/app/components/booked-rooms/booked-rooms.component.spec.ts
+++ src/app/components/booked-rooms/booked-rooms.component.spec.ts
@@ -1,10 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookedRoomsComponent } from './booked-rooms.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('BookedRoomsComponent', () => {
   let component: BookedRoomsComponent;
   let fixture: ComponentFixture<BookedRoomsComponent>;
+
+  // class Util {
+  //   static get emptyResponse(): DebugElement {
+  //     return fixture.debugElement.query(By.css('[data-role="booked-rooms-empty-response"]'));
+  //   }
+  // }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,10 +21,15 @@ describe('BookedRoomsComponent', () => {
 
     fixture = TestBed.createComponent(BookedRoomsComponent);
     component = fixture.componentInstance;
+    component.bookings = [];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should display proper message if no bookings', () => {
+  //   expect(Util.emptyResponse.nativeElement.content).toContain('You don’t have any reservation');
+  // });
 });
